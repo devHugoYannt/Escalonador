d60fcc726b897fbc40f0f4125c98c15f9b739f07
Tech Priest <131812189+Tech-Priest1@users.noreply.github.com>
2025-09-11 10:44:52 -0300

    Update escalonador.ts
    removendo redundância

--- escalonador.ts
+++ escalonador.ts
@@ -1,34 +1,5 @@
 
-
-class Processo {
-  id: number;
-  quantidadeInstrucoes: number;
-
-  constructor(id: number, quantidadeInstrucoes: number) {
-    this.id = id;
-    this.quantidadeInstrucoes = quantidadeInstrucoes;
-  }
-
-  executarInstrucao(): void {
-    if (this.quantidadeInstrucoes > 0) {
-      this.quantidadeInstrucoes--;
-    }
-  }
-
-  estaFinalizado(): boolean {
-    return this.quantidadeInstrucoes === 0;
-  }
-}
-
-class GeradorDeProcessos {
-  private static proximoId: number = 1;
-
-  static gerar(): Processo {
-    const id = this.proximoId++;
-    const quantidadeInstrucoes = Math.floor(Math.random() * (50 - 10 + 1)) + 10;
-    return new Processo(id, quantidadeInstrucoes);
-  }
-}
+import { Processo, GeradorDeProcessos } from "./processos";
 
 
 
